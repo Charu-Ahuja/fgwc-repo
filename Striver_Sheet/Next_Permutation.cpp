3077682fd8ed7aa33bc9124152f4983e8ff210c2
Charu Ahuja <off.charu.ahuja01@gmail.com>
2026-02-04 21:23:27 +0530

    day1-completed

---
+++ Striver_Sheet/Next_Permutation.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    // TC->O(N)
+    // SC->O(1)
+    void nextPermutation(vector<int> &nums)
+    {
+        int n = nums.size();
+        int idx = -1;
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                idx = i;
+                break;
+            }
+        }
+        if (idx == -1)
+        {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (nums[i] > nums[idx])
+            {
+                swap(nums[i], nums[idx]);
+                break;
+            }
+        }
+        reverse(nums.begin() + idx + 1, nums.end());
+    }
+};
