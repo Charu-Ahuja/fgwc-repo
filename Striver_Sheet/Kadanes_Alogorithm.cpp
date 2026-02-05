e072b765444bf776e441c814625dcce97943b7d8
Charu Ahuja <off.charu.ahuja01@gmail.com>
2026-02-05 17:57:05 +0530

    5 th Feb Done

--- Striver_Sheet/Kadanes_Alogorithm.cpp
+++ Striver_Sheet/Kadanes_Alogorithm.cpp
@@ -1,28 +1,39 @@
-//  Brute Force method to print all the subarrays for a given array is :->
-// TC -> O(n^3)
-// SC ->O(1)
-
 #include <iostream>
+#include <climits>
+#include <algorithm>
 using namespace std;
-void Print_all_Subarrays(int arr[], int n)
+
+void Print_Max_Subarray_Sum(int arr[], int n)
 {
+    int maxSum = INT_MIN;
+
     for (int i = 0; i < n; i++)
     {
         for (int j = i; j < n; j++)
         {
+            int currSum = 0;
+
             for (int k = i; k <= j; k++)
             {
-                cout << arr[k];
+                cout << arr[k] << " ";
+                currSum += arr[k];
             }
-            cout << " ";
+
+            cout << " -> Sum = " << currSum << endl;
+
+            maxSum = max(maxSum, currSum);
         }
         cout << endl;
     }
+
+    cout << "Maximum Subarray Sum is : " << maxSum << endl;
 }
+
 int main()
 {
-    int arr[6] = {1, 3, 7, 9, 0, 2};
-    int n = 6;
-    Print_all_Subarrays(arr, n);
+    int arr[7] = {3, -4, 5, 4, -1, 7, -8};
+    int n = 7;
+
+    Print_Max_Subarray_Sum(arr, n);
     return 0;
 }
